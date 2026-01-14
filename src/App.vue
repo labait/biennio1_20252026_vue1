48f928b293fcb8bb02038c473e0c5b661a36d32b
Antonio Molinari <antoniomolinari@me.com>
2026-01-14 15:58:34 +0100

    add Accordion component and integrate it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup>
 import About from './components/About.vue'
+import Accordion from './components/Accordion.vue';
 import Projects from './components/Projects.vue'
 import Footer from './components/Footer.vue';
+import { onMounted } from 'vue';
+
+
 </script>
 
 <template>
   <main class="container mx-auto p-4 pt-8">
     <About class="mb-4"/>
+    <Accordion class="mb-4"/>
     <Projects />
     <Footer />
   </main>
---
+++ src/components/Accordion.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  document.querySelectorAll('.accordion').forEach(accordion => {
+    accordion.querySelectorAll('.accordion-title').forEach(title => {
+      title.addEventListener('click', () => {
+        accordion.querySelectorAll('.accordion-content').forEach(content => {
+          content.classList.add('hidden');
+        });
+        const content = title.nextElementSibling;
+        if (content) {
+          content.classList.toggle('hidden');
+        }
+      });
+    });
+  });
+});
+</script>
+
+<template>
+  <!-- accordion -->
+  <div class="accordion">
+    <div>
+      <button class="accordion-title w-full p-4 cursor-pointer bg-amber-50">Section 1</button>
+      <div class="accordion-content hidden">
+        <p>This is the content of section 1.</p>
+      </div>
+    </div>
+    <div>
+      <button class="accordion-title w-full p-4 cursor-pointer bg-amber-50">Section 1</button>
+      <div class="accordion-content hidden">
+        <p>This is the content of section 1.</p>
+      </div>
+    </div>
+  </div>
+</template>
