234edb1a92c5614ee767e6817bdbb2b43a2052fd
Antonio Molinari <antoniomolinari@me.com>
2026-01-14 15:11:09 +0100

    add Footer component and integrate it into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import About from './components/About.vue'
 import Projects from './components/Projects.vue'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <main class="container mx-auto p-4 pt-8">
     <About class="mb-4"/>
     <Projects />
+    <Footer />
   </main>
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="text-center text-md text-gray-500 mt-8">
+    &copy; 2024 Your Name. All rights reserved.
+  </footer>
+</template>
