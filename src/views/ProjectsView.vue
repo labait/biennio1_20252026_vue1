3b9e1ee16b21ddfb824a4310f33453151273efd5
Antonio Molinari <antoniomolinari@me.com>
2026-01-21 16:06:35 +0100

    feat: refactor ProjectsView component to use script setup and improve template structure

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,12 +1,12 @@
+<script setup>
+import Projects from '../components/Projects.vue';
+</script>
+
 <template>
-  <div>
+  <div class="mb-6">
     <h1>Projects</h1>
     <p>Questa è la pagina Projects.</p>
   </div>
+  <Projects />
 </template>
 
-<script>
-export default {
-  name: 'ProjectsView',
-};
-</script>
